dd7741c70f7b65f577480590fde202b6d36c0dea
Tiago <tgnvsctn@gmail.com>
2024-02-16 18:21:15 -0300

    feat: configured home page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,19 @@
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { ThemeProvider, DefaultTheme } from "@react-navigation/native";
+import * as NavigationBar from "expo-navigation-bar";
 import { SplashScreen, Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import * as SystemUI from "expo-system-ui";
+import { Platform } from "react-native";
 
 import { base, dark } from "@/constants/DesignTokens";
 
 SystemUI.setBackgroundColorAsync(dark.bg.canvas);
 SplashScreen.preventAutoHideAsync();
+
+if (Platform.OS === "android") {
+  NavigationBar.setBackgroundColorAsync(dark.bg.canvas);
+}
 
 const theme = {
   ...DefaultTheme,
