1be5fdf643876d5cb9d5e6092e692966406dbde1
Tiago <tgnvsctn@gmail.com>
2024-02-16 12:09:34 -0300

    chore: add font

---
+++ src/app/(app)/_layout.tsx
@@ -0,0 +1,33 @@
+import {
+  useFonts,
+  PlusJakartaSans_400Regular,
+  PlusJakartaSans_700Bold,
+} from "@expo-google-fonts/plus-jakarta-sans";
+import { Stack, SplashScreen } from "expo-router";
+import { useEffect } from "react";
+
+export default function AppLayout() {
+  const [loaded, error] = useFonts({
+    PlusJakartaSans_400Regular,
+    PlusJakartaSans_700Bold,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
+  // This layout can be deferred because it's not the root layout.
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    ></Stack>
+  );
+}
