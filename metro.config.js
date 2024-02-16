95262488c703c981bf5b8059e3d0ba2e6672dd7d
Tiago <tgnvsctn@gmail.com>
2024-02-16 12:50:25 -0300

    chore: add suport for svgs

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,19 @@ const { getDefaultConfig } = require("expo/metro-config");
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.resolver.assetExts = config.resolver.assetExts.filter(
+  (ext) => ext !== "svg"
+);
+config.resolver.sourceExts.push("svg", "mjs", "cjs");
+
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: true
+  }
+});
+config.transformer.babelTransformerPath = require.resolve(
+  "react-native-svg-transformer"
+);
+
 module.exports = config;
